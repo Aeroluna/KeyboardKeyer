7f8c810fb42dc839f993ced834bceab56435e78d
Kinsi <kinsi55@users.noreply.github.com>
2021-07-29 20:12:33 +0200

    Prevent SongCore from Reloading when Pressing R while a Keyboard is active

--- KeyboardKeyer/UIKeyboard.cs
+++ KeyboardKeyer/UIKeyboard.cs
@@ -2,15 +2,26 @@
 {
     using HarmonyLib;
     using HMUI;
+    using System;
+    using System.Reflection;
 
     [HarmonyPatch(typeof(UIKeyboard))]
     [HarmonyPatch(nameof(UIKeyboard.Awake))]
     internal static class BeatmapDataLoaderGetBeatmapDataFromBeatmapSaveData
     {
+        internal static KeyboardKeyerController keyerInstance;
+
         private static void Postfix(UIKeyboard __instance)
         {
-            KeyboardKeyerController keyboardKeyer = __instance.gameObject.AddComponent<KeyboardKeyerController>();
-            keyboardKeyer.Init(__instance);
+            keyerInstance = __instance.gameObject.AddComponent<KeyboardKeyerController>();
+            keyerInstance.Init(__instance);
         }
+    }
+
+    [HarmonyPatch]
+    internal static class BlockSongCoreRefresh {
+        static MethodBase TargetMethod() => AccessTools.Method("SongCore.Loader:Update");
+        static Exception Cleanup(Exception _) => null;
+        static bool Prefix() => BeatmapDataLoaderGetBeatmapDataFromBeatmapSaveData.keyerInstance?.isActiveAndEnabled != true;
     }
 }
